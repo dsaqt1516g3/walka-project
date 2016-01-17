aedefe92c37b97bc82240622c4ee02fa6625c410
sergiogm4 <sgonzalezmontilla4@gmail.com>
2016-01-17 23:51:09 +0100

    Database for production

--- service/src/main/java/edu/upc/eetac/dsa/walka/db/Database.java
+++ service/src/main/java/edu/upc/eetac/dsa/walka/db/Database.java
@@ -6,28 +6,39 @@ import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 /**
  * Created by SergioGM on 05.12.15.
  */
 public class Database {
+    private static Database instance = null;
+    private DataSource ds;
 
-        private static Database instance = null;
-        private DataSource ds;
-
-        private Database() {
-            HikariConfig config = new HikariConfig(Database.class.getClassLoader().getResource("hikari.properties").getFile());
-            ds = new HikariDataSource(config);
+    private Database() {
+        PropertyResourceBundle prb = (PropertyResourceBundle) ResourceBundle.getBundle("hikari");
+        Enumeration<String> keys = prb.getKeys();
+        Properties properties = new Properties();
+        while(keys.hasMoreElements()){
+            String key = keys.nextElement();
+            properties.setProperty(key, prb.getString(key));
         }
 
-        private final static Database getInstance() {
-            if (instance == null)
-                instance = new Database();
-            return instance;
-        }
+        //HikariConfig config = new HikariConfig(Database.class.getClassLoader().getResource("hikari.properties").getFile());
+        HikariConfig config = new HikariConfig(properties);
+        ds = new HikariDataSource(config);
+    }
 
-        public final static Connection getConnection() throws SQLException {
-            return getInstance().ds.getConnection();
-        }
+    private final static Database getInstance() {
+        if (instance == null)
+            instance = new Database();
+        return instance;
+    }
 
+    public final static Connection getConnection() throws SQLException {
+        return getInstance().ds.getConnection();
+    }
 }
